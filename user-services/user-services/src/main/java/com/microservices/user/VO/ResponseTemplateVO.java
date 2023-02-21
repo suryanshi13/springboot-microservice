47e71c0318b38a1298cb80c4417871b212002052
suryanshi13 <91493849+suryanshi13@users.noreply.github.com>
2023-02-21 13:18:53 +0530

    user service

---
+++ user-services/user-services/src/main/java/com/microservices/user/VO/ResponseTemplateVO.java
@@ -0,0 +1,14 @@
+package com.microservices.user.VO;
+
+import com.microservices.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseTemplateVO {
+    private User user;
+    private Department department;
+}
